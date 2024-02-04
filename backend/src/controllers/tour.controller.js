4c0cd959d84665137966a722b84f35efbba6bf82
Bidyut-Kr-Das <bkdas2017.bd@gmail.com>
2024-02-05 00:47:51 +0530

    documentation added for api

--- backend/src/controllers/tour.controller.js
+++ backend/src/controllers/tour.controller.js
@@ -5,13 +5,27 @@ import { Tour } from "#models/tours.model.js";
 //get all tours
 export const getAllTours = async (req, res) => {
   try {
-    const filteredQuery = { ...req.query };
+    //Build the query
+    const originalQuery = { ...req.query };
     const excludingQueries = ["field", "page", "sort", "limit"];
     excludingQueries.forEach((queryElement) => {
-      delete filteredQuery[queryElement];
+      delete originalQuery[queryElement];
     });
 
-    const tours = await Tour.find(filteredQuery);
+    //converting the object into string then using regex to find all the occurence in that string \b checks for exact match and g at the end of regex finds all occurence the replace command returns the matched string with replaced string
+
+    let filteredQuery = JSON.stringify(originalQuery);
+    filteredQuery = filteredQuery.replace(/\b(gt|gte|lt|lte)\b/g, (match) => {
+      return `$${match}`;
+    });
+    // console.log(JSON.parse(filteredQuery));
+
+    const query = Tour.find(JSON.parse(filteredQuery)); //<-- As model.find() returns a query and we can chain this with multiple option and later await for the query to get executed and return the result
+
+    //Executing the query by awaiting for it
+    const tours = await query;
+
+    //Sending the response
     res.status(200).json({
       status: "success",
       results: tours.length,
---
+++ readme.md
@@ -0,0 +1,44 @@
+# Natours Website
+
+- developed by [Bidyut Kr. Das](https://github.com/Bidyut-Kr-Das)
+
+> Frontend under development
+
+> Backend Api guide
+
+- database is hosted on mongoDB
+- api features
+
+  - > http method - GET
+
+    - `returns all tours`
+
+    ```bash
+    /api/v1/tours
+    ```
+
+    - `returns searched field that matches with the value `
+
+    ```bash
+    /api/v1/tours?field=value
+    ```
+
+    - `returns searched field that is greater than the value`
+      available options
+      `gt` = greater than
+      `lt` = less than
+      `gte` = greater than equal to
+      `lte` = less than equal to
+      works in all number fields
+
+    ```bash
+    /api/v1/tours?field[gt]=value
+    ```
+
+  - > http method - POST
+
+    - `Creates a new tour(validation applied)`
+
+      ```bash
+      /api/v1/tours
+      ```
